890f01ee7257eed3c067dc3828c38e6dbc258c73
root <muthami293>
2022-10-18 11:58:15 -0700

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,6 +24,14 @@ void times_table(void)
 				_putchar(' ');
 				_putchar('0' + product);
 			}
+			else if (product > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+_putchar('0' + (product % 10));
+			}
 		}
+		_putchar('\n');
 	}
 }
