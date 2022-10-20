3f770381c819406bcc11d05252c5d6f67f2179a4
root <muthami293>
2022-10-20 11:32:13 -0700

    largest_number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -16,21 +16,4 @@ void positive_or_negative(int i);
  * Return: largest number
  */
 
-int largest_number(int a, int b, int c)
-{
-	int largest;
-
-	if (a > b && b > c)
-	{
-		largest = a;
-	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
-	return (largest);
-}
+int largest_number(int a, int b, int c);
