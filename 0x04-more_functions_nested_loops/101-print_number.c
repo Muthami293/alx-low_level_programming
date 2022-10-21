2ef10a1bf1284c761d921eea221d999939d052a5
root <muthami293>
2022-10-21 14:26:36 -0700

    print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * print_number - Print the integer number
+  * @n: number to check
+  * Return: void
+  */
+
+void print_number(int n)
+{
+	unsigned int tens, digit, number = n;
+	double bucket = 1;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			number = n * -1;
+			_putchar('-');
+		}
+		while (bucket <= number)
+			bucket *= 10;
+		tens = bucket / 10;
+
+		while (tens >= 1)
+		{
+			digit = number / tens;
+			_putchar(digit + '0');
+			number = (number - (tens * digit));
+			tens /= 10;
+		}
+	}
+}
