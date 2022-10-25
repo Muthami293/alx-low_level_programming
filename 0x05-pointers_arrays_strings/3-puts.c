44b69241cce6e6944f6485b12ca7f49030ceeafa
root <muthami293>
2022-10-25 10:22:56 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints the string
+ * @str: string parameter
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	
+	while (*str != '\0')
+	{
+		_putchar(*(str++));
+	}
+
+	_putchar('\n');
+}
