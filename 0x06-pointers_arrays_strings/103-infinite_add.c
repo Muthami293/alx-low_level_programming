8c8071b632db528a0d51f90a5c451bad3d6d11b1
root <muthami293>
2022-10-28 09:08:05 -0700

    infinite add

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * infinite_add - two characters from buffer of size r
+  * @n1: character 1
+  * @n2: character 2
+  * @r: the buffer
+  * @size_r: size of buffer
+  * Return: pointer to result
+  */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+}
