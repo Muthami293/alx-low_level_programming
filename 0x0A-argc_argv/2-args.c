b7a7778726286a5a6cfe614664bb4fc28dfcb92d
root <muthami293>
2022-11-08 00:58:18 -0800

    Prints all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - Prints all arguments
+  * @argc: argument count
+  * @argv: pointer to arguments
+  *
+  * Return: 0
+  */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int i;
+
+	for (i = 0; i >= 0; i++)
+	{
+		printf("%d\n", argc);
+	}
+
+	return (0);
+}
