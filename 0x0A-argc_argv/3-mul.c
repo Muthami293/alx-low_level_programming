05f96dc9858495798fd474b5042aa8f13e686a44
root <muthami293>
2022-11-09 01:07:48 -0800

    Multiplication of two arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Prints all arguments
+  * @argc: argument count
+  * @argv: pointer to arguments
+  *
+  * Return: 0 for success, 1 for error
+  */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+	
+	printf("argc = %d\n", argc);
+
+	for (i = 1; i < argc; i++)
+	{
+		if (argc != 2)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			printf("%s\n", argv[i]);
+			mul = mul * atoi(argv[i]);
+		}
+		printf("Total =%d\n", mul);
+	}
+
+	return (0);
+}
