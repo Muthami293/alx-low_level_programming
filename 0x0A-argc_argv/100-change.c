b50abe4ac82a140706a8ec806e42b7aca0be49fa
root <muthami293>
2022-11-11 08:36:03 -0800

    Change given back

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - print minimum no of coins to make change
+  * @argc: argment count
+  * @argv: argument vector
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int coins = 0;
+	int cents;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+
+		coins += 1;
+	}
+
+	printf("%i\n", coins);
+
+	return (0);
+
+}
