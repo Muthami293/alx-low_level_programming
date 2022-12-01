41ee6b7eae7eb623db49093cfbf3b45a66f42712
root <muthami293>
2022-12-01 09:45:20 -0800

    initial

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+  * pop_listint - delete head node and return the data n
+  * @head: node to delete and return its data
+  * Return: data in head node or 0 if list is empty
+  */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int data;
+
+	if (*head == NULL)
+		return (0);
+
+	temp = *head;
+	data = temp->n;
+
+	*head = temp->next;
+
+	free(temp);
+
+	return (data);
+
+}
