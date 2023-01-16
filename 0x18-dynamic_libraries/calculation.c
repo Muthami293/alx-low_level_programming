dfd45e0230993a9b3109bd3aca6195a915bf8c1f
root <muthami293>
2023-01-16 07:01:21 -0800

    c file

---
+++ 0x18-dynamic_libraries/calculation.c
@@ -0,0 +1,62 @@
+#include "calc.h"
+
+/**
+  * add - function to add two numbers
+  * @a: first operand
+  * @b: second operand
+  * Return: sum
+  */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+  * sub - function to subracts two numbers
+  * @a: first operand
+  * @b: second operand
+  * Return: remainder
+  */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * mul - function to multiply two numbers
+  * @a: first operand
+  * @b: second operand
+  * Return: product
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * div - function to divide two numbers
+  * @a: first operand
+  * @b: second operand
+  * Return: quotient
+  */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * mod - function find mod of two nums
+  * @a: first operand
+  * @b: second operand
+  * Return: remainder
+  */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
